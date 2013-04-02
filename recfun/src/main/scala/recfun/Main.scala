2f9e2b374fbb7d01c1a0785f3e2f3bf926315dc6
Devil <devil@ncdc.pl>
2013-04-03 00:35:00 +0200

    Week01 Assingment 1.2 was implemented

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -2,30 +2,53 @@ package recfun
 import common._
 
 object Main {
-  def main(args: Array[String]) {
-    println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
+    def main(args: Array[String]) {
+        println("Pascal's Triangle")
+        for (row <- 0 to 10) {
+            for (col <- 0 to row)
+                print(pascal(col, row) + " ")
+            println()
+        }
     }
-  }
 
-  /**
-   * Exercise 1
-   */
-  def pascal(c: Int, r: Int): Int = {
-      if ((0 == c) || (0 == r) || (c == r)) 1 
-      else pascal(c - 1, r - 1) + pascal(c, r - 1)
-  }
+    /**
+     * Exercise 1
+     */
+    def pascal(c: Int, r: Int): Int = {
+        if ((0 == c) || (0 == r) || (c == r)) 1
+        else pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
 
-  /**
-   * Exercise 2
-   */
-  def balance(chars: List[Char]): Boolean = ???
+    /**
+     * Exercise 2
+     */
+    def balance(chars: List[Char]): Boolean = {
 
-  /**
-   * Exercise 3
-   */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+        def balanced(p_openCount: Int, chars: List[Char]): Boolean = {
+            if (0 > p_openCount) false
+            else {
+                if (true == chars.isEmpty)
+                    if (0 == p_openCount) true
+                    else false
+                else {
+                    val currentChar: Char = chars.head
+                    if (')' == currentChar)
+                        balanced(p_openCount - 1, chars.tail)
+                    else if ('(' == currentChar)
+                        balanced(p_openCount + 1, chars.tail)
+                    else
+                        balanced(p_openCount, chars.tail)
+                }
+            }
+        }
+
+        balanced(0, chars)
+    }
+
+    /**
+     * Exercise 3
+     */
+    def countChange(money: Int, coins: List[Int]): Int = {
+        0
+    }
 }
